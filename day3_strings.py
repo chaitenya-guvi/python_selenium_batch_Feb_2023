1a19f39db6c5f4b6609960cd4b32736506feaff6
chaitenya-guvi <115387942+chaitenya-guvi@users.noreply.github.com>
2023-02-18 13:52:40 +0530

    Add files via upload

---
+++ day3_strings.py
@@ -0,0 +1,45 @@
+# strings
+
+# escape characters  : \n - newline  , \b  ,  \t - tab
+
+string1 = "Thenameis:    Chaitenya "
+string2 = "The name is : \n \nChaitenya "
+string3 = "Thenameis:\tline3Chaitenya "
+string4 = "The name is : \\ Chaitenya "
+string5 = "The name is : \" Chaitenya \""
+string6 = 'The name is : \b Chaitenya \''
+
+path = "C:\\Users\\LENOVO\\Desktop\\guvi_tutorial_feb_2023\\day3_strings.py"
+/\/\/\
+print(path)
+
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+print(string5)
+print(string6)
+
+
+
+
+
+
+
+# "double quote and single quote "
+
+#
+# string1 = "example 1"
+# string_2 = 'example 2'
+# print(string1)
+# print(string_2)
+# string_3 = "while giving the speec the CM said 'Hello There people'"
+# string_4 = 'while giving the speec the CM said "Hello There people"'  # expecting an error
+#
+# print(string_3)
+# print(string_4)
+#
+
+
+
