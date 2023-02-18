6a74981454510fadaeee6986eb760dbfc087a595
chaitenya-guvi <115387942+chaitenya-guvi@users.noreply.github.com>
2023-02-18 12:43:32 +0530

    Add files via upload

---
+++ day2_notes.py
@@ -0,0 +1,74 @@
+# variable are containers of some values  , they are meant to used later in the code
+# variable name on the left and assign the value using an =
+# firstname = "chaitenya"
+age = 22
+print(type(age))
+is_adult = True
+is_adult_false = False
+print(type(is_adult))
+# booleans are usually used for flags , or status
+is_power = True
+print(is_power)
+is_power_off = False
+# Dynamic typing
+firstname = "chaitenya"
+lastname = "Kumar"
+
+middle_name = None  # placeholder , that you define it early in code and then update it later
+
+list1 = [1, "2", None, 1.5]
+dict1 = {"key": "value", "firstname": "chaitenya"}  # key value pair
+"""
+Data types - integer ,, float ,, strings ,, Boolean , Nonetype , complex numbers 
+Complex data types also , data structures  - list , tuple , sets ,dictionaries 
+"""
+
+
+
+print(firstname)
+print(age)
+print(type(firstname))
+
+
+"""
+Rules \Guidelines for variable names  : 
+1. Variable Name should not be a exiting python function or keyword 
+2. Variables must start with a letter or underscore
+3. Names are case-sensitive 
+
+"""
+
+"""
+Excercise 
+create 3 variables  ---- 1. college name , 2 whether you passed out ,3. the year that you passed out in 
+"""
+
+"""
+Conventions  : 
+
+Naming Conventions
+
+Most variables should be snake_case (underscores between words)
+Variables that start and end with two underscores (called "dunder" for double underscore)
+    are supposed to be private or left alone
+Most variables should be also be lowercase, with some exceptions:
+UpperCamelCase usually refers to a class 
+Identifiers cannot be a keyword.
+Identifiers are case-sensitive.
+It can have a sequence of letters and digits. 
+However, it must begin with a letter or _. 
+The first letter of an identifier cannot be a digit.
+It's a convention to start an identifier with a letter rather _.
+
+Whitespaces are not allowed.
+We cannot use special symbols like !, @, #, $, and so on.
+CAPITAL_SNAKE_CASE usually refers to constants (e.g. PI = 3.14)
+
+
+"""
+
+"""
+Dynamic Typing 
+"""
+
+a= 2
---
+++ day3_comparison_operators.py
@@ -0,0 +1,32 @@
+# arithmetic operators ( + , - , * , // , PEMDAS)
+# Comparison operators (
+# logical operators
+
+"""
+==	Is Equal To	3 == 5 gives us False
+!=	Not Equal To	3 != 5 gives us True
+>	Greater Than	3 > 5 gives us False
+<	Less Than	3 < 5 gives us True
+>=	Greater Than or Equal To	3 >= 5 give us False
+<=	Less Than or Equal To	3 <= 5 gives us True
+"""
+first_variable = 5
+second_variable = 2
+
+# equal to operator
+print('first_variable == second_variable =', first_variable == second_variable)
+
+# not equal to operator
+print('first_variable != second_variable =', first_variable != second_variable)
+
+# greater than operator
+print('first_variable > second_variable =', first_variable > second_variable)
+
+# less than operator
+print('first_variable < second_variable =', first_variable < second_variable)
+
+# greater than or equal to operator
+print('first_variable >= second_variable =', first_variable >= second_variable)
+
+# less than or equal to operator
+print('first_variable <= second_variable =', first_variable <= second_variable)
